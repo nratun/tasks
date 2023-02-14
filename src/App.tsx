eabfdab3eff7efec7dd00ffe991e47883e56cfb1
Rana <rantun@udel.edu>
2023-02-13 19:39:15 -0500

    Added columns and red rectangles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import image from "./images/baby.jpeg";
 import { Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function App(): JSX.Element {
     return (
@@ -10,17 +13,46 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript - Rana Tuncer
             </header>
             <h1>
-                <span style={{ color: "blue" }}>This is me on discord</span>
+                <span style={{ color: "blue" }}>My cool header</span>
             </h1>
-            <img src={image} alt="My discord profile pic" />
-            List of My Favorite Foods:
-            <ul>
-                <li>Chicken Tikka Masala</li>
-                <li>Soup</li>
-                <li>Pasta</li>
-            </ul>
+            <Container>
+                <Row>
+                    <Col>
+                        <div
+                            style={{
+                                border: "1px solid red",
+                                padding: "4px",
+                                width: "223px",
+                                height: "35px",
+                                backgroundColor: "red"
+                            }}
+                        >
+                            Here{"'"}s my discord picture:
+                        </div>
+                        <img src={image} alt="My discord profile pic" />
+                    </Col>
+                    <Col>
+                        <div
+                            style={{
+                                border: "1px solid red",
+                                padding: "4px",
+                                width: "223px",
+                                height: "35px",
+                                backgroundColor: "red"
+                            }}
+                        >
+                            List of My Favorite Foods:
+                            <ul>
+                                <li>Chicken Tikka Masala</li>
+                                <li>Soup</li>
+                                <li>Pasta</li>
+                            </ul>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
             <div>
-                <Button onClick={() => console.log("I am logged")}>
+                <Button onClick={() => console.log("Hello World!")}>
                     Log Hello World
                 </Button>
             </div>
