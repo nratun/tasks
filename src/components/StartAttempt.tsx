ff7ddd06e6d9830986bd8350437cbfcc4ee7c1a4
Rana <rantun@udel.edu>
2023-03-06 19:33:02 -0500

    Working on StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,21 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [status, setStatus] = useState<boolean>(false);
+    const [attempts, setAttempts] = useState<number>(4);
+    function changeStatus(): void {
+        setStatus(status === false ? true : false);
+    }
+    return (
+        <div>
+            <div>
+                <Button onClick={changeStatus} disabled={status}>
+                    Start Quiz
+                </Button>
+                <Button onClick={changeStatus} disabled={!status}>
+                    Stop Quiz
+                </Button>
+            </div>
+        </div>
+    );
 }
