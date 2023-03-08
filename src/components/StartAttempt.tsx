d15b05281e8e2a5fa7cffb0f480877f185b1ead3
Rana <rantun@udel.edu>
2023-03-08 12:46:15 -0500

    Finished StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -7,15 +7,26 @@ export function StartAttempt(): JSX.Element {
     function changeStatus(): void {
         setStatus(status === false ? true : false);
     }
+    function incAttempts(): void {
+        setAttempts(attempts + 1);
+    }
+    function dec_change(): void {
+        setAttempts(attempts - 1);
+        setStatus(status === false ? true : false);
+    }
     return (
         <div>
             <div>
-                <Button onClick={changeStatus} disabled={status}>
+                <Button onClick={dec_change} disabled={status || attempts <= 0}>
                     Start Quiz
                 </Button>
                 <Button onClick={changeStatus} disabled={!status}>
                     Stop Quiz
                 </Button>
+                <Button onClick={incAttempts} disabled={status}>
+                    Mulligan
+                </Button>
+                <span> {attempts} </span>
             </div>
         </div>
     );
