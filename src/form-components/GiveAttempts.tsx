cb8d4f5f49888481086e0cdbf176646edf8df961
Rana <rantun@udel.edu>
2023-03-24 11:55:29 -0400

    Still figuring out GiveAttempts

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,8 +1,22 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [attempts, setAttempts] = useState<number>(3);
+    const [numRequest, setNumRequests] = useState<number>(0);
+    
     return (
         <div>
+            <Form.Group controlId="formGiveAttempts">
+                <Form.Label>Add More Attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAttempts(attempts + numRequest)
+                    }
+                />
+            </Form.Group>
             <h3>Give Attempts</h3>
         </div>
     );
