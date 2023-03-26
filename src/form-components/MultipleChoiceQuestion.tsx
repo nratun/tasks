188b90a54c5935c32c43b76e97e7c10dc5c99aac
Rana <rantun@udel.edu>
2023-03-26 10:21:56 -0400

    Finished all questions, fix formatting

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,28 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [currChoice, setChoice] = useState<string>(options[0]);
+
+    function updateChoice(event: React.ChangeEvent<HTMLSelectElement>) {
+        setChoice(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <div>
+                <Form.Group controlId="userChoices">
+                    <Form.Label>Pick an option from the dropdown</Form.Label>
+                    <Form.Select value={currChoice} onChange={updateChoice}>
+                        {options.map((anOption) => (
+                            <option key={anOption} value={anOption}>
+                                {anOption.toUpperCase()}
+                            </option>
+                        ))}
+                    </Form.Select>
+                </Form.Group>
+            </div>
+            <span>{currChoice == expectedAnswer ? "✔️" : "❌"}</span>
         </div>
     );
 }
